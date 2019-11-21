039e6cd723b0e99624f61049f47597db5bddf08b
Hidden Brains <hb@Dimple09.local>
2019-11-21 13:50:32 +0530

    minor changes

--- src/mod_test.erl
+++ src/mod_test.erl
@@ -6,7 +6,11 @@
 
 -behaviour(gen_mod).
 
--export([start/2, stop/1, process_iq_now/1, user_exists/2]).
+-export([start/2, 
+	stop/1, 
+	process_iq_now/1,
+	user_exists/2]).
+
 -export([md5_hex/1]).
 
 -define(PROCNAME, ?MODULE).
