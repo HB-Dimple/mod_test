9381070bad45fe2be3b85701cd9bae02b67437e6
Hidden Brains <hb@Dimple09.local>
2019-11-21 13:14:44 +0530

    minor changes

--- src/mod_test.erl
+++ src/mod_test.erl
@@ -1,7 +1,24 @@
 -module(mod_test).
--behaviour(gen_mod).
+
 -include("logger.hrl").
--export([start/2, stop/1, depends/2, mod_options/1]).
+-include("xmpp.hrl").
+-include("mod_roster.hrl").
+
+-behaviour(gen_mod).
+
+-export([start/2, stop/1, process_iq_now/1, user_exists/2]).
+-export([md5_hex/1]).
+
+-define(PROCNAME, ?MODULE).
+-define(NS_OPENPGP, <<"jabber:e2eencryption">>).
+
+-record(query, {group_jid = <<>> :: binary(), 
+	other_jid = <<>> :: binary()}).
+
+get_mod(<<"query">>, <<"jabber:e2eencryption">>) ->
+	mod_test.
+get_mod({query, _, _}) -> mod_test.
+
 start(_Host, _Opts) ->
     ?INFO_MSG("Hello, ejabberd world!", []),
     ok.
